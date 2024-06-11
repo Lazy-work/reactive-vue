daf642c8114c60671c1bb1a68a8db83d91cc7bfb
NGBAMA William <william.ngbama@gmail.com>
2024-06-11 10:02:40 +0200

    fix: handle function declaration on return

--- packages/babel-optimizer/src/index.js
+++ packages/babel-optimizer/src/index.js
@@ -2,22 +2,18 @@ let t;
 
 const optimizeFnProp = {
   JSXAttribute(path) {
-    const expression = path.get('value.expression');
+    const expression = path.get("value.expression");
     if (expression.isArrowFunctionExpression() || expression.isFunctionExpression()) {
-      if (this.seen.includes(path.node)) return;
-      this.seen.push(path.node);
-      const cbId = path.scope.generateUidIdentifier('cb');
-      const cbVar = t.variableDeclaration('const', [t.variableDeclarator(cbId, expression.node)]);
-      const returnSt = path.findParent((path) => path.isReturnStatement() || path.isVariableDeclaration());
-      if (!returnSt) return;
-      returnSt.insertBefore(cbVar);
+      const cbId = path.scope.generateUidIdentifier("cb");
+      const cbVar = t.variableDeclaration("const", [t.variableDeclarator(cbId, expression.node)]);
+      this.componentReturn.insertBefore(cbVar);
       expression.replaceWith(cbId);
     }
-  },
+  }
 };
 
 const isDirtyTraverse = {
-  'ArrowFunctionExpression|FunctionExpression': {
+  "ArrowFunctionExpression|FunctionExpression": {
     enter(path) {
       if (!this.ignoredBlock) {
         this.ignoredBlock = path.node;
@@ -29,7 +25,7 @@ const isDirtyTraverse = {
         this.ignoredBlock = null;
         this.ignore = false;
       }
-    },
+    }
   },
   JSXExpressionContainer: {
     enter(path) {
@@ -41,7 +37,7 @@ const isDirtyTraverse = {
       if (this.toInspect.includes(path.node)) {
         this.observe = false;
       }
-    },
+    }
   },
   MemberExpression(path) {
     if (this.observe && !this.ignore) {
@@ -54,7 +50,7 @@ const isDirtyTraverse = {
       this.dirty();
       path.stop();
     }
-  },
+  }
 };
 
 function isDirty(path) {
@@ -74,8 +70,8 @@ function isDirty(path) {
   return dirty;
 }
 
-function moveChildren(parent, dirtiness) {
-  const p = parent.findParent((path) => path.isReturnStatement());
+function moveChildren(parent, dirtiness, componentReturn) {
+  const p = componentReturn;
   if (!p) return;
   const children = parent.node.children;
 
@@ -84,16 +80,16 @@ function moveChildren(parent, dirtiness) {
     if (!t.isJSXElement(child) && !t.isJSXFragment(child)) continue;
     if (dirtiness.get(child)) continue;
     if (t.isJSXExpressionContainer(child)) child = child.expression;
-    const jsxId = parent.scope.generateUidIdentifier('jsx');
+    const jsxId = parent.scope.generateUidIdentifier("jsx");
     const jsxDec = t.variableDeclarator(jsxId, child);
-    const jsxVar = t.variableDeclaration('const', [jsxDec]);
+    const jsxVar = t.variableDeclaration("const", [jsxDec]);
     p.insertBefore(jsxVar);
     parent.get(`children.${i}`).replaceWith(t.JSXExpressionContainer(jsxId));
   }
 }
 
 const optimizeStatic = {
-  'ArrowFunctionExpression|FunctionExpression': {
+  "ArrowFunctionExpression|FunctionExpression": {
     enter(path) {
       if (!this.ignoredBlock) {
         this.ignoredBlock = path.node;
@@ -105,13 +101,12 @@ const optimizeStatic = {
         this.ignoredBlock = null;
         this.ignore = false;
       }
-    },
+    }
   },
   JSXElement: {
     enter(path) {
+      console.log(path.node)
       if (this.ignore) return;
-      if (this.seen.includes(path.node)) return;
-      this.seen.push(path.node);
       if (!this.dirtiness.has(path.node)) this.dirtiness.set(path.node, false);
 
       const parent = this.parents[this.parents.length - 1];
@@ -134,91 +129,118 @@ const optimizeStatic = {
       const isDirty = this.dirtiness.get(path.node);
 
       if (isDirty) {
-        moveChildren(path, this.dirtiness);
+        moveChildren(path, this.dirtiness, this.componentReturn);
       }
-    },
-  },
+    }
+  }
 };
 
 const breakDownReturn = {
   JSXElement: {
     exit(path) {
-      if (this.seen.includes(path.node)) return;
-      this.seen.push(path.node);
-
       const program = path.findParent((path) => path.isProgram());
-      this.rsxIdentifier = program.scope.generateUidIdentifier('rsx');
+      this.rsxIdentifier = program.scope.generateUidIdentifier("rsx");
 
-      program.unshiftContainer('body', [
-        t.importDeclaration(
-          [t.importSpecifier(this.rsxIdentifier, t.identifier('rsx'))],
-          t.stringLiteral('@lazywork/reactive-vue-jsx-runtime'),
-        ),
-      ]);
+      if (!this.rsxIdentifier) {
+        program.unshiftContainer("body", [
+          t.importDeclaration(
+            [t.importSpecifier(this.rsxIdentifier, t.identifier("rsx"))],
+            t.stringLiteral("@lazywork/reactive-vue-jsx-runtime")
+          )
+        ]);
+      }
 
       const container = path.findParent((path) => path.isJSXExpressionContainer());
       const isInFunctionCall = path
         .findParent((path) => (container && path.node === container.node) || path.isCallExpression())
         .isCallExpression();
       if (container && isInFunctionCall) return;
-      const props = path.get('openingElement');
-      props.traverse(optimizeFnProp, { types: t, seen: this.seen });
+      const props = path.get("openingElement");
+      props.traverse(optimizeFnProp, { types: t, seen: this.seen, componentReturn: this.componentReturn });
 
       const cbJsx = t.arrowFunctionExpression([], path.node);
 
-      const cbId = path.scope.generateUidIdentifier('cbJsx');
-      const cbVar = t.variableDeclaration('const', [t.variableDeclarator(cbId, cbJsx)]);
+      const cbId = path.scope.generateUidIdentifier("cbJsx");
+      const cbVar = t.variableDeclaration("const", [t.variableDeclarator(cbId, cbJsx)]);
 
-      const parent = path.findParent((path) => path.isReturnStatement() || path.isVariableDeclaration());
-
-      if (!parent) return;
-      parent.insertBefore(cbVar);
+      this.componentReturn.insertBefore(cbVar);
       if (path.parentPath.isJSXElement() || path.parentPath.isJSXFragment()) {
         path.replaceWith(t.JSXExpressionContainer(t.callExpression(this.rsxIdentifier, [cbId])));
       } else {
         path.replaceWith(t.callExpression(this.rsxIdentifier, [cbId]));
       }
-    },
-  },
+    }
+  }
 };
+
+const findReturns = {
+  "ArrowFunctionExpression|FunctionExpression|FunctionDeclaration": {
+    enter(path) {
+      if (!this.ignoredBlock) {
+        this.ignoredBlock = path.node;
+        this.ignore = true;
+      }
+    },
+    exit(path) {
+      if (this.ignoredBlock === path.node) {
+        this.ignoredBlock = null;
+        this.ignore = false;
+      }
+    }
+  },
+  ReturnStatement(path) {
+    if (this.ignore) return;
+    this.returns.push(path);
+  }
+}
 
 const handleReactiveComponent = {
   ReturnStatement(path) {
     const t = this.types;
-    const argument = path.get('argument');
-    if (argument.isJSXElement()) {
+    const argument = path.get("argument");
+    if (this.componentReturn.node === path.node && argument.isJSXElement()) {
       argument.replaceWith(t.arrowFunctionExpression([], argument.node));
     }
-    let jsx = argument.get('body');
-    jsx.traverse(optimizeStatic, { seen: [], dirtiness: new WeakMap(), parents: [] });
-    jsx.traverse(breakDownReturn, { seen: [] });
-  },
+    const body = argument.get('body');
+    let jsxList;
+    if (body.isBlockStatement()) {
+      const returns = [];
+      body.traverse(findReturns, { returns });
+      jsxList = returns;
+    } else {
+      jsxList = [body];
+    }
+    for (const jsx of jsxList) {
+      jsx.traverse(optimizeStatic, { dirtiness: new WeakMap(), parents: [], componentReturn: this.componentReturn });
+      jsx.traverse(breakDownReturn, { componentReturn: this.componentReturn });
+    }
+  }
 };
+
 let rsxIdentifier;
 export default function (babel) {
   const { types } = babel;
   t = types;
-  const seen = [];
   return {
-    name: 'reactive-vue-auto',
+    name: "reactive-vue-auto",
     visitor: {
       Program(path) {
         if (!rsxIdentifier) {
-          rsxIdentifier = path.scope.generateUidIdentifier('rsx');
+          rsxIdentifier = path.scope.generateUidIdentifier("rsx");
 
-          path.unshiftContainer('body', [
-            t.importDeclaration(
-              [t.importSpecifier(rsxIdentifier, t.identifier('rsx'))],
-              t.stringLiteral('@lazywork/jsx-runtime-vue'),
-            ),
+          path.unshiftContainer("body", [
+            t.importDeclaration([t.importSpecifier(rsxIdentifier, t.identifier("rsx"))], t.stringLiteral("@lazywork/jsx-runtime-vue"))
           ]);
         }
       },
       CallExpression(path) {
-        if (path.node.callee.name === 'reactivity') {
-          path.traverse(handleReactiveComponent, { types: t, seen });
+        if (path.node.callee.name === "reactivity") {
+          const componentBody = path.get('arguments.0.body');
+          const returnIndex = componentBody.node.body.findIndex((item) => t.isReturnStatement(item))
+          const componentReturn = componentBody.get(`body.${returnIndex}`)
+          path.traverse(handleReactiveComponent, { types: t, componentReturn });
         }
-      },
-    },
+      }
+    }
   };
 }
