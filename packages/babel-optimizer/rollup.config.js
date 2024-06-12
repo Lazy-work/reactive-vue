bbcdcf99bc99f7c3bd0dafedcc9073c96e57235c
NGBAMA William <william.ngbama@gmail.com>
2024-06-12 05:53:04 +0200

    fix(optimizer): convert package to common js

--- packages/babel-optimizer/rollup.config.js
+++ packages/babel-optimizer/rollup.config.js
@@ -1,36 +1,22 @@
 import nodeResolve from "@rollup/plugin-node-resolve";
 import replace from '@rollup/plugin-replace';
-import esbuild from "rollup-plugin-esbuild";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = fileURLToPath(new URL(".", import.meta.url));
+import commonjs from '@rollup/plugin-commonjs';
 
 const plugins = [
   nodeResolve({
     extensions: [".js", ".ts"],
-    browser: true,
   }),
   replace({
     '__DEV__': `!!(process.env.NODE_ENV !== 'production')`,
     preventAssignment: true
   }),
-  esbuild({
-    tsconfig: path.resolve(__dirname, "tsconfig.json"),
-    sourceMap: true,
-    target: "esnext"
-  }),
+  commonjs(),
 ];
 
 export default [
   {
     input: "src/index.js",
     output: [
-      {
-        file: "dist/babel-optimizer.esm-bundler.js",
-        format: "es",
-        sourcemap: true,
-      },
       {
         file: "dist/babel-optimizer.cjs.js",
         format: "cjs",
