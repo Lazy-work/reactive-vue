0506c47b38a0a756c3f0403bb0f7a434a8ca8701
NGBAMA William <william.ngbama@gmail.com>
2024-11-23 08:16:03 +0100

    refactor(core): remove custom reactivity implementation

--- packages/core/src/constants.ts
+++
@@ -1,6 +0,0 @@
-
-export const IS_GLOBAL_KEY = Symbol('__v_isGlobal');
-export const INITIAL_CONTEXT_KEY = Symbol('__v_initialContext');
-export const CONTEXT_KEY = Symbol('__v_context');
-export const SUBSCRIBE_KEY = Symbol('__v_subscribe');
-export const LISTENER_LIST_KEY = Symbol('__v_listenerList');
